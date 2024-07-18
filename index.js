99b78ad1c17313ba388a100654a4c7b4f00ad9ea
vivekbarvadiya <vivekbarvadiya5@email.com>
2024-07-18 16:11:04 +0530

    index changed

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const app = express();
+// 7318591601:AAHjaw9sSkfXs0dwgbS0dLZiBl6do0RLaII
 
-const PORT = 5000
+const token = '7318591601:AAHjaw9sSkfXs0dwgbS0dLZiBl6do0RLaII';
+let TelegramBot = require('node-telegram-bot-api');
+let bot = new TelegramBot(token, { polling: true });
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: "Its / root"
-    });
-})
+// Matches "/echo [whatever]"
+bot.onText(/\/echo(.+)/, (msg, match) => {
 
-app.listen(PORT, (req, res) => {
-    console.log('its working')
-})
+	// The 'msg' is the received Message from Telegram
+	// and 'match' is the result of executing the regexp
+	// above on the text content of the message
+
+	let chatId = msg.chat.id;
+
+	// The captured "whatever"
+	let resp = match[1];
+
+	// send back the matched "whatever" to the chat
+	bot.sendMessage(chatId, resp);
+});
